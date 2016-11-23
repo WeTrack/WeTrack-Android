2206e875af6854f793e13140e5615d2ceeb767c0
Robert Peng <robert.peng@foxmail.com>
2016-11-23 20:55:27 +0800

    Slightly refactored ChatActivity and ChatMessageAdapter

--- app/src/main/java/com/wetrack/ChatActivity.java
+++ app/src/main/java/com/wetrack/ChatActivity.java
@@ -2,7 +2,7 @@ package com.wetrack;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,7 +13,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
-import com.wetrack.Adaptor.ChatMessageAdaptor;
+import com.wetrack.adapter.ChatMessageAdapter;
 import com.wetrack.model.Chat;
 import com.wetrack.model.ChatMessage;
 import com.wetrack.model.User;
@@ -24,19 +24,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ChatActivity extends FragmentActivity implements View.OnClickListener {
-    private Toolbar toolbar;
-    private ListView listView;
-    private EditText mEditTextContent;
-    private Button buttonSend;
-    private RelativeLayout edittext_layout;
-    private ChatMessageAdaptor adapter;
-    private InputMethodManager manager;
-    //List<String> AmyMessage;
-    //List<String> BobMessage;
-    private User me;
-    private User user1;
-    private User user2;
+public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
+    private ListView messageListView;
+    private EditText messageEditText;
+    private RelativeLayout messageEditLayout;
+    private ChatMessageAdapter adapter;
+    private InputMethodManager imeManager;
     private List<ChatMessage> chatMessageList;
 
 
@@ -45,58 +38,48 @@ public class ChatActivity extends FragmentActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         initView();
-        setUpView();
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_send:
-                // 点击发送按钮(发文字)
-                String s = mEditTextContent.getText().toString();
-                sendText(s);
-                break;
-            default:
-                break;
+        if (v.getId() == R.id.send_btn) {
+            String s = messageEditText.getText().toString();
+            sendMessage(s);
         }
     }
 
     private void initView() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
-        listView = (ListView) findViewById(R.id.list);
-        mEditTextContent = (EditText) findViewById(R.id.et_sendmessage);
-        buttonSend = (Button) findViewById(R.id.btn_send);
+
+        Button buttonSend = (Button) findViewById(R.id.send_btn);
         buttonSend.setOnClickListener(this);
-        edittext_layout = (RelativeLayout) findViewById(R.id.edittext_layout);
-        edittext_layout.setBackgroundResource(R.drawable.input_bar_bg_normal);
-        edittext_layout.requestFocus();
-        mEditTextContent.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+        messageListView = (ListView) findViewById(R.id.message_list_view);
+        messageEditText = (EditText) findViewById(R.id.message_edit);
+
+        messageEditLayout = (RelativeLayout) findViewById(R.id.message_edit_layout);
+        messageEditLayout.setBackgroundResource(R.drawable.input_bar_bg_normal);
+        messageEditLayout.requestFocus();
+
+        messageEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    edittext_layout
-                            .setBackgroundResource(R.drawable.input_bar_bg_active);
-                } else {
-                    edittext_layout
-                            .setBackgroundResource(R.drawable.input_bar_bg_normal);
-                }
-
+                if (hasFocus)
+                    messageEditLayout.setBackgroundResource(R.drawable.input_bar_bg_active);
+                else
+                    messageEditLayout.setBackgroundResource(R.drawable.input_bar_bg_normal);
             }
         });
-    }
 
-    private void setUpView() {
-        manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        //test
+        imeManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         Chat chatTest = new Chat();
         User user1 = new User();
@@ -106,53 +89,60 @@ public class ChatActivity extends FragmentActivity implements View.OnClickListen
         user2.setUsername("Bob");
         user2.setIconUrl("head2.png");
         chatTest.setChatId("chatId1");
-        List<String> usernames = new ArrayList<String>();
+        List<String> usernames = new ArrayList<>();
         usernames.add(user1.getUsername());
         usernames.add(user2.getUsername());
         chatTest.setMemberNames(usernames);
         ChatMessage defaultMessage = new ChatMessage();
         defaultMessage.setChatId(chatTest.getChatId());
         defaultMessage.setFromUsername(user1.getUsername());
-        defaultMessage.setContent("This is a default message sent by Amy. lalallalalalallalalalalalalalallallalalalallalalalalallalalalallalalalalalalalallallalalalallalalalalallalalalallalalalalalalalallallalalalallalala");
+        defaultMessage.setContent("This is a default message sent by Amy.");
         defaultMessage.setSendTime(LocalDateTime.parse("2016-11-23"));
 
-        chatMessageList = new ArrayList<ChatMessage>();
-        chatMessageList.add(defaultMessage);
-        adapter = new ChatMessageAdaptor(this, chatMessageList, chatTest, user2);
-        listView.setAdapter(adapter);
-        listView.setOnTouchListener(new View.OnTouchListener() {
+        chatMessageList = new ArrayList<>();
+        adapter = new ChatMessageAdapter(this, chatMessageList, chatTest, user2);
+        messageListView.setAdapter(adapter);
+        messageListView.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 hideKeyboard();
-                edittext_layout.requestFocus();
+                messageEditLayout.requestFocus();
                 return false;
             }
         });
+        messageListView.setTranscriptMode(ListView.TRANSCRIPT_MODE_NORMAL);
 
+        // Test on receive message
+        onReceiveMessage(defaultMessage);
     }
 
     public void editClick(View v) {
-        listView.setSelection(listView.getCount() - 1);
+        messageListView.setSelection(messageListView.getCount() - 1);
     }
 
     private void hideKeyboard(){
         if (getWindow().getAttributes().softInputMode != WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN) {
             if (getCurrentFocus() != null)
-                manager.hideSoftInputFromWindow(getCurrentFocus()
+                imeManager.hideSoftInputFromWindow(getCurrentFocus()
                         .getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         }
     }
 
-    private void sendText(String s){
-        //test
+    private void onReceiveMessage(ChatMessage message) {
+        chatMessageList.add(message);
+        adapter.refresh(chatMessageList);
+        messageListView.setSelection(messageListView.getCount() - 1);
+    }
+
+    private void sendMessage(String message){
         ChatMessage sendMessage = new ChatMessage();
         sendMessage.setFromUsername("Bob");
-        sendMessage.setSendTime(LocalDateTime.parse("2016-11-23"));
-        sendMessage.setContent(s);
+        sendMessage.setSendTime(LocalDateTime.now());
+        sendMessage.setContent(message);
         chatMessageList.add(sendMessage);
         adapter.refresh(chatMessageList);
-        listView.setSelection(listView.getCount() - 1);
-        mEditTextContent.setText("");
+        messageListView.setSelection(adapter.getCount() - 1);
+        messageEditText.setText("");
     }
 }
---
+++ app/src/main/java/com/wetrack/adapter/ChatMessageAdapter.java
@@ -0,0 +1,107 @@
+package com.wetrack.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.wetrack.R;
+import com.wetrack.model.Chat;
+import com.wetrack.model.ChatMessage;
+import com.wetrack.model.User;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.List;
+
+
+public class ChatMessageAdapter extends BaseAdapter {
+    private static final DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd hh:mm");
+
+    private Context context;
+
+    private User currentUser;
+    private Chat chat;
+    private List<ChatMessage> chatMessageList;
+
+    public ChatMessageAdapter(Context context, List<ChatMessage> chatMessageList,
+                              Chat chat, User currentUser){
+        this.context = context;
+        this.chat = chat;
+        this.currentUser = currentUser;
+        this.chatMessageList = chatMessageList;
+    }
+
+    public void refresh(List<ChatMessage> chatMessageList) {
+        this.chatMessageList = chatMessageList;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return chatMessageList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.chat_message_item, parent, false);
+
+        LinearLayout leftLayout = (LinearLayout) row.findViewById(R.id.left_layout);
+        LinearLayout rightLayout = (LinearLayout) row.findViewById(R.id.right_layout);
+        // Received message
+        if (!chatMessageList.get(position).getFromUsername().equals(currentUser.getUsername())) {
+            TextView timestamp = (TextView) row.findViewById(R.id.timestamp);
+            TextView content = (TextView) row.findViewById(R.id.left_msg);
+            ImageView portrait = (ImageView) row.findViewById(R.id.tv_userhead);
+
+            timestamp.setText(chatMessageList.get(position).getSendTime().toString(formatter));
+            content.setText(chatMessageList.get(position).getContent());
+            rightLayout.setVisibility(View.GONE);
+            leftLayout.setVisibility(View.VISIBLE);
+
+
+            List<String> chatUsers = chat.getMemberNames();
+            for(int i = 0; i < chatUsers.size(); i++ ){
+                if(chatMessageList.get(position).getFromUsername() == chatUsers.get(i)) {
+                    //从本地数据库查找username为message.getFromUsername()的User fromUser
+                    //以用fromUser.getIconUrl()获得fromUser的头像
+                    break;
+                }
+            }
+            //这里直接使用drawable的head1.png头像
+            portrait.setBackgroundResource(R.drawable.head1);
+            return row;
+        }
+        // Sent message
+        else {
+            TextView timestamp = (TextView) row.findViewById(R.id.timestamp);
+            TextView content = (TextView) row.findViewById(R.id.right_msg);
+            ImageView portrait = (ImageView) row.findViewById(R.id.iv_userhead);
+            timestamp.setText(chatMessageList.get(position).getSendTime().toString(formatter));
+            content.setText(chatMessageList.get(position).getContent());
+            leftLayout.setVisibility(View.GONE);
+            rightLayout.setVisibility(View.VISIBLE);
+
+            // 用currentUser.getIconUrl()currentUser头像
+            // 这里直接使用head2.png
+            portrait.setBackgroundResource(R.drawable.head2);
+            return row;
+        }
+    }
+}
